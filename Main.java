00b69cc1abfce9d7cb3ae230e76f63a55faf473e
Beka Bugianishvili <44806920+beqabeka1@users.noreply.github.com>
2020-10-05 18:50:42 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,47 @@
+package BTU;
+
+
+//	import java.util.Scanner;
+//    import java.lang.Math;
+//    public class Main {
+//
+//    public static void main(String[] args) {
+//        int a, b, c;
+//        System.out.println("Enter the numbers:");
+//        Scanner scanner = new Scanner(System.in);
+//        a = scanner.nextInt();
+//        b = scanner.nextInt();
+//        c = scanner.nextInt();
+//        System.out.println("Your numbers are:");
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        a =  Math.abs(a) % 10;
+//        System.out.println("First number's last digit is:\t"+a);
+//        b =  Integer.parseInt(String.valueOf(Math.abs(b)).substring(0,1));
+//        System.out.println("Second number's first digit is:\t"+b);
+//        c = String.valueOf(c).chars().map(Character::getNumericValue).sum();
+//        System.out.println("Sum of numbers\t"+c);
+//        System.out.println(a*b);
+//        System.out.println(b+(a*b));
+//
+//    }
+//}
+
+//    public class Main{
+//        public static void main(String[] args) {
+//            int x,y;
+//            System.out.println("Enter number X:");
+//            Scanner scanner = new Scanner(System.in);
+//            x = scanner.nextInt();
+//            System.out.println("X+12="+(x+12));
+//            if (x % 2 == 0)
+//                System.out.println("X-luwia");
+//            else
+//                System.out.println("X-kentia");
+//            System.out.println("Enter number Y:");
+//            y = scanner.nextInt();
+//            System.out.println("X+Y="+(x+y));
+//
+//        }
+//    }
